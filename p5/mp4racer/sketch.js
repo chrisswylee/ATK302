8d5139e27b1543da1e02aa7ae38aae1f10d6c8d3
chrisswylee <cwlee12@ilstu.edu>
2019-12-03 13:43:10 -0600

    mp4

--- p5/mp4racer/sketch.js
+++ p5/mp4racer/sketch.js
@@ -1,90 +1,184 @@
+// Landscape Example
+// Coding for Artists - 2019
+// Instructor: Urban Reininger
+var myState = 0;
+var tree1, tree2; //Chris
+var sign1, sign2; //Chris
+var retroracer; //Chris
+var song1; //Chris
+var daysunpos = 200;
+var nightsunpos = 200;
+var cars = [];
+var timer = 0;
+var carPos;
+
+
+
+function preload() {
+  song1 = loadSound("assets/lazerhawk.mp3"); //Chris song1
+
+}
 
 function setup() {
+
+
+  tree1 = loadImage("assets/tree.png");
+  tree2 = loadImage("assets/treesmall.png");
+  sign1 = loadImage("assets/sign.png");
+  sign2 = loadImage("assets/signsmall.png");
+  retroracer = loadImage("assets/retroracer.png");
+
+  imageMode(CENTER);
+
+
   createCanvas(windowWidth, windowHeight);
+
+  // for (var i = 0; i < 5; i++) {
+  //
+  //   cars.push(new Car());
+  //
+  // }
+
+
+
+  carPos = createVector(709, 600);
+
+
 } // end setup
 
-function draw() {
-  background(159, 189, 237);// rgb  0-255
 
-  // pretty sky
-  for(var i=0; i<=500; i+=5){
-    strokeWeight(5);
-    stroke(215-i,128-i,64);
-    line(0,350-i,width,350-i); // x,y,x,y
+function draw() {
+
+
+
+
+  switch (myState) {
+    case 0:
+
+
+      break;
+
+    case 1:
+      push();
+
+      nighttime();
+
+      pop();
+
+      timer++;
+      if (timer > 100) {
+        cars.push(new Car());
+        timer = 0;
+      }
+      for (var i = 0; i < cars.length; i++) { // Chris DRIVE
+
+        cars[i].display();
+        cars[i].drive();
+
+        if (cars[i].pos.y > height) {
+
+          cars.splice(i, 1);
+
+
+        }
+      }
+      image(retroracer, carPos.x, carPos.y, 500 * carPos.y / 600, 500 * carPos.y / 560);
+      checkForKeys();
+      break;
+
+    case 2:
+      daylight();
+
+      break;
+
+    case 3:
+
+      break;
+
+    case 4:
+
+      break;
+
+
+  }
+}
+
+
+
+function mouseReleased() {
+  myState++;
+  daysunpos = 200;
+  nightsunpos = 200;
+  if (myState > 2) myState = 0;
+}
+
+function mouseLocation() {
+  fill(255);
+  text(("x: " + mouseX), 20, 10);
+  text(("y: " + mouseY), 20, 20);
+} // end mouseLocation
+
+function Car() {
+  //attribute
+
+  if (random(2) > 1) {
+    this.pos = createVector(random(700, 800), 235);
+    this.velocity = createVector(random(-1, -1), random(1, 1));
+  } else {
+    this.pos = createVector(random(1400, 1300), 235);
+    this.velocity = createVector(random(5, 5), random(1, 1));
+
+  }
+  //   if (this.pos.x < 640) {
+  //
+  // } else {
+  //
+  //   this.velocity = createVector(random(1, 5), random(1, 5)) ;
+  // }
+
+
+
+  //methods
+  this.display = function() {
+
+    //
+    // fill(this.r, this.g, this.b) ;
+    // rect(this.pos.x, this.pos.y, 100, 100) ;
+    image(tree1, this.pos.x, this.pos.y, 300 * this.pos.y / 80, 800 * this.pos.y / 250);
+
+
+
+
   }
 
-  // sun
-  noStroke();
-  fill(250,186, 3);
-  ellipse(700,(100+frameCount/10),70,70); 1
-  //text(frameCount,200,150);
+  this.drive = function() {
+    this.pos.add(this.velocity);
+    // if (this.pos.x > width) this.pos.x = 0 ;
+    // if (this.pos.x < 0) this.pos.x = width ;
+    //if (this.pos.y > height) this.pos.y = 350;
 
-  // mountains
+  }
 
-  // cloud
-  fill(255,255,255,50);
-  ellipse(frameCount/4,150,40,20);
-//cloud 2
-  fill(255,255,255,50);
-  ellipse(frameCount/6,95,40,20);
+}
 
-  // Green Ground
-  noStroke();
-  fill(0,100,0);// rgb
-  rect(0,350,width,1000);
-  //street
-  fill('#6E6C6C');
- quad(660,350, 740, 350, 1175, 1000, 275, 1000);
 
-  // left tree 1
-  fill(67, 51, 8); // tree brown
-  rect(420,350,10,20); // trunk
-  fill(23, 69, 29);
-  ellipse(426,372,20,20);// tree top
-  ellipse(424,392,30,20);
-  ellipse(430,382,20,20);
-  //right tree1
-  fill(67, 51, 8); // tree brown
-  rect(920,194,10,20); // trunk
-  fill(23, 69, 29);
-  ellipse(926,172,20,20);// tree top
-  ellipse(924,192,30,20);
-  ellipse(930,182,20,20);
+function checkForKeys() {
+  if (keyIsDown(LEFT_ARROW)) carPos.x = carPos.x - 15;
+  if (keyIsDown(RIGHT_ARROW)) carPos.x = carPos.x + 15;
 
-  // tree 2
-  push();
-    translate(-40,-40);
-    scale(1.5);
-    fill(67, 51, 8); // tree brown
-    rect(320,394,10,20); // trunk
-    fill(23, 69, 29);
-    ellipse(326,392,20,20);// tree top
-    ellipse(324,372,30,20);
-    ellipse(330,382,20,20);
-    pop();
 
-push();
-    translate(-40,-40);
-    scale(1.5);
-    fill(67, 51, 8); // tree brown
-    rect(720,194,10,20); // trunk
-    fill(23, 69, 29);
-    ellipse(726,192,20,20);// tree top
-    ellipse(724,172,30,20);
-    ellipse(730,182,20,20);
+  if ((keyIsDown(UP_ARROW)) && (carPos.y > 325)) {
+    carPos.y = carPos.y - 15;
+  }
 
-  pop();
+  if ((keyIsDown(DOWN_ARROW)) && (carPos.y < height - 30)) {
+    carPos.y = carPos.y + 15;
 
-  //left tree 2
+  }
 
 
 
 
-  mouseLocation();
-} // end draw
 
-function mouseLocation(){
-  fill(255);
-  text(("x: " + mouseX),20,10);
-  text(("y: " + mouseY),20,20);
-} // end mouseLocation
+
+}
